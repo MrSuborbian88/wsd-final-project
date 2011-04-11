bc2c0e055d33085a3ae8a5d593aaa6c85f7d7255
MrSuborbian88 <MrSuborbian88@gmail.com>
2011-04-11 16:58:17 +0000

    Made slight changes to the database, added data, and added error output to php collect script.

--- web/collect_articles.php
+++ web/collect_articles.php
@@ -14,9 +14,12 @@
  echo mysql_error();
 
  $rss = simplexml_load_file($rss_address);
+
  foreach($rss->channel->item as $item)
  {
    $sql = 'INSERT INTO rss_articles(guid, url,title,description,pubdate) VALUES (\''.mysql_real_escape_string($item->guid).'\',\''.mysql_real_escape_string($item->link).'\',\''.mysql_real_escape_string($item->title).'\',\''.strip_tags(mysql_real_escape_string($item->description)).'\',\''.date("Y-m-d h:i:s",strtotime(mysql_real_escape_string($item->pubDate))).'\');';
-   mysql_query($sql);
+ mysql_query($sql);
+ echo mysql_error();
  }
+
 ?>
