bfd0d186cf4bce0e955b09c180894c275a4d0e3e
MrSuborbian88 <MrSuborbian88@gmail.com>
2011-04-13 21:00:06 +0000

    Updated sample data and the most recent links list in final_project.php

--- final_project.php
+++ final_project.php
@@ -40,6 +40,8 @@
  $db = mysql_connect($server, $user , $password);
  mysql_select_db($database);
  echo mysql_error();
+ /*
+ //Temporary, get 5 most recent articles
  $sql = 'SELECT * FROM rss_articles ORDER BY pubdate DESC LIMIT 5';
  $results = mysql_query($sql);
  $row = mysql_fetch_array( $results);
@@ -51,6 +53,30 @@
 		$abridged_title .= '...';
 	echo '<li><a href="'.$row['url'].'" title="'.$title.'">'.$abridged_title."</a></li>";
 		$row = mysql_fetch_array( $results );
+	}
+  */
+ //Get user's most recent clicks
+ $user_id = 0; //CHANGE TO GET ACTUAL USER
+ $sql = 'SELECT DISTINCT user_id, article_id FROM clicks WHERE user_id = ' . strval($user_id) . ' ORDER BY clicktime DESC LIMIT 5;';
+ $results = mysql_query($sql);
+ $row = mysql_fetch_array( $results);
+ while($row != null)
+	{
+	$sql = 'SELECT * FROM rss_articles WHERE id = '. strval($row['article_id']) . ';';
+	$article_result = mysql_query($sql);
+	$article_row = mysql_fetch_array($article_result);
+	while($article_row != null)
+	{
+        $title = $article_row['title'];
+	if($title == null || $title == '')
+		$title = $article_row['url']; //After testing, change to $article_row['title']
+	$abridged_title = substr($title, 0, 27);
+	if(strlen($title) > 26)
+		$abridged_title .= '...';
+	echo '<li><a href="'.$article_row['url'].'" title="'.$title.'">'.$abridged_title."</a></li>";
+		$article_row = mysql_fetch_array( $article_result );
+	}
+	$row = mysql_fetch_array( $results );
 	}
   ?>
      </ul>
