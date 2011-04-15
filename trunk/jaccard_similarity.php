cdb5ceefb255257ae6846f68f8edae74a90d1c4f
MrSuborbian88@gmail.com <MrSuborbian88@gmail.com@5868701f-a4c1-9795-2333-7f8349baeec4>
2011-04-15 02:00:17 +0000

    Added a few fixes to jaccard_similarity function

--- trunk/jaccard_similarity.php
+++ trunk/jaccard_similarity.php
@@ -1,20 +1,18 @@
-<?PHP
+<?php
 
 function jaccard_similarity($arr1, $arr2)
 {
 
   $s1 = sizeof($arr1);
   $s2 = sizeof($arr2);
-  $size = $s1 + $s2;
-
-  foreach ($arr1 as $val)
-  {
-      if(in_array($val, $arr2)
-        {
-          $similarity++;
-        }
-  }
-
-  $distance = 1 - ($similarity/$size);
+  $d1 = sizeof(array_diff($arr1, $arr2));
+  $d2 = sizeof(array_diff($arr2, $arr1));
+  $union = $s1 + $d2;  //Set Difference
+  $intersection = ($union) - ($d1 + $d2); //Set Intersection
+  $distance = 1 - ($intersection/$union);
+  return $distance;
 }
 ?>
+
+
+
