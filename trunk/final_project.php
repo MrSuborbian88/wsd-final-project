0291f1bafc7eee669abee056b9ecdbe0763e4a41
MrSuborbian88@gmail.com <MrSuborbian88@gmail.com@5868701f-a4c1-9795-2333-7f8349baeec4>
2011-04-19 00:52:57 +0000

    Added article select for user cluster.  Also fixed some of the login redirect issues.

--- trunk/final_project.php
+++ trunk/final_project.php
@@ -7,8 +7,8 @@
 <body>
   <div id="wrap">
   <div id = "login">
-    <a href = "http://localhost/CAS_RPI.php">Login</a>
-    <a href = "http://localhost/CAS_RPI_logout.php">Logout</a>
+    <a href = "CAS_RPI.php">Login</a>
+    <a href = "CAS_RPI_logout.php">Logout</a>
   </div>
   <div id="header">
   <h1>Morning Mail Recommender <span class = "span_header">RENSSELAER POLYTECHNIC INSTITUTE - Troy, NY, USA</span></h1>
@@ -22,28 +22,47 @@
  $db = mysql_connect($server, $user , $password);
  mysql_select_db($database);
  echo mysql_error();
- $sql = 'SELECT * FROM rss_articles ORDER BY pubdate DESC LIMIT 5';
+ $username = ""; //However we're getting username
+ $cluster = -1;
+ $sql = 'SELECT cluster FROM users WHERE username=\'' . $username .'\';';
  $results = mysql_query($sql);
  $row = mysql_fetch_array( $results);
  while($row != null)
+ {
+	$cluster = $row['cluster'];
+	$row = mysql_fetch_array( $results );
+ }
+ //User not clustered, show 10 most recent
+ if($cluster <= 0)
+ {
+ $sql = 'SELECT * FROM rss_articles ORDER BY pubdate DESC LIMIT 5';
+ $results = mysql_query($sql);
+ $row = mysql_fetch_array( $results);
+  while($row != null)
 	{
 		echo '<h3>'.'<a href="'.$row['url'].'">'.$row['title'].'</a>'.'</h3>';
 		echo '<p>'.$row['description'].'</p>';
 		$row = mysql_fetch_array( $results );
 	}
+ }
+ else
+ {
+ $sql = 'SELECT article_id, a.title, a.url, a.description, count(*) FROM clicks c INNER JOIN rss_articles a ON c.article_id = a.id WHERE EXISTS (SELECT 1 FROM users u WHERE u.cluster = '.strval($cluster).') GROUP BY c.article_id ORDER BY count(*) DESC LIMIT 5';
+ $results = mysql_query($sql);
+ $row = mysql_fetch_array( $results);
+  while($row != null)
+	{
+		echo '<h3>'.'<a href="'.$row['url'].'">'.$row['title'].'</a>'.'</h3>';
+		echo '<p>'.$row['description'].'</p>';
+		$row = mysql_fetch_array( $results );
+	}
+ }
   ?>
     </div>
     <div id="nav">
     <h4>Recently Viewed</h4>
     <ul>
 <?php
- $server = 'localhost';
- $user = '';
- $password = '';
- $database = 'test';
- $db = mysql_connect($server, $user , $password);
- mysql_select_db($database);
- echo mysql_error();
  /*
  //Temporary, get 5 most recent articles
  $sql = 'SELECT * FROM rss_articles ORDER BY pubdate DESC LIMIT 5';
@@ -73,7 +92,7 @@
 	{
         $title = $article_row['title'];
 	if($title == null || $title == '')
-		$title = $article_row['url']; //After testing, change to $article_row['title']
+		$title = $article_row['url'];
 	$abridged_title = substr($title, 0, 27);
 	if(strlen($title) > 26)
 		$abridged_title .= '...';
