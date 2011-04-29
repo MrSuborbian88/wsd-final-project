22ba4ee3d6a1742d5a339a75182163ffaf805552
MrSuborbian88@gmail.com <MrSuborbian88@gmail.com@5868701f-a4c1-9795-2333-7f8349baeec4>
2011-04-29 20:34:49 +0000

    Added code to prevent insertion of multiple clicks for the same article

--- trunk/click.php
+++ trunk/click.php
@@ -4,5 +4,5 @@ require_once('dbcon.php');
 $article = mysql_real_escape_string($_POST['id']);
 $user = mysql_real_escape_string($_POST['user']);
 
-$q = "INSERT INTO clicks (user_id, article_id) VALUES ($user, $article)";
+$q = "INSERT INTO clicks (user_id, article_id) SELECT DISTINCT $user, $article FROM clicks c WHERE NOT EXISTS (SELECT 1 FROM clicks WHERE user_id = $user AND article_id = $article)";
 mysql_query($q);
